64ed42feaa00eb9df22ddbaf5cffd83d49435b03
SlinkyWalnut <bolaji.o.alabi@icloud.com>
2024-10-26 19:13:59 -0400

    Fixed config/.env

---
+++ say-it/src/Backend/config.js
@@ -0,0 +1,8 @@
+
+const CONFIG = {
+    GoogleFirebaseAPIKey : "AIzaSyDxZ6F2Ww3EOvME-IiQVDhRmeyO_frCWDE",
+    AuthentificationId : "sayit-2af2e.firebaseapp.com",
+    ProjectId : "sayit-2af2e"
+};
+
+export default CONFIG
--- say-it/src/Backend/firebase.js
+++ say-it/src/Backend/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, serverTimestamp } from "firebase/firestore";
+import CONFIG from './config.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +9,13 @@ import { getFirestore, serverTimestamp } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.GoogleFirebaseAPIKey,
-  authDomain: process.env.AuthentificationId,
-  projectId: process.env.ProjectId,
+  apiKey: CONFIG.GoogleFirebaseAPIKey,
+  authDomain: CONFIG.AuthentificationId,
+  projectId: CONFIG.ProjectId,
   storageBucket: "sayit-2af2e.appspot.com",
   messagingSenderId: "567470998924",
-  appId: "1:567470998924:web:f2f1a358b9c96cf95102a9",
-  measurementId: "G-ELG0YFV91C"
+  appId: "1:567470998924:web:a1800929a7f82a8f5102a9",
+  measurementId: "G-ZLWYQX2MPL"
 };
 
 const app = initializeApp(firebaseConfig);
